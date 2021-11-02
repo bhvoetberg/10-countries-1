7b8c235fe1249fe69bcefe0d319eda2e2a0cef36
bhvoetberg <b.h.voetberg@gmail.com>
2021-11-02 17:53:10 +0100

    Array komt binnen

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+import axios from "axios";
+
 console.log('Hallo daar!');
+
+async function getCountries() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all');
+        // console.log(result.data[0].name);
+        // console.log(result.data[0].population);
+        // console.log(result.data[0].region);
+        // console.log(result.data);
+        const countriesContainer = document.getElementById('countries');
+        countriesContainer.textContent = result.data.value;
+        return result.data;
+    } catch(e) {
+        console.error(e);
+    }
+}
+//
+let info = getCountries();
+console.log(info[0].name);
